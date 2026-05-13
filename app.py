407d11a8b3371bb48bad52d40856cf77aacd3123
SUJI-01092006 <suji.g2024aids@sece.ac.in>
2026-05-13 14:08:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("Hello CI Pipeline")
+def calculate_grade(mark):
+    if mark >= 90:
+        return "A Grade"
+    elif mark >= 75:
+        return "B Grade"
+    elif mark >= 50:
+        return "C Grade"
+    else:
+        return "Fail"
+
+student_name = "Suji"
+mark = 87
+
+grade = calculate_grade(mark)
+
+print("===== Student Grade Report =====")
+print("Student Name :", student_name)
+print("Student Mark :", mark)
+print("Student Grade:", grade)
+print("CI Pipeline Build Successful")
